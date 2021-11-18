461eb16f340fb8989d9598686ee40ed08c595b8e
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-11-17 21:28:30 -0800

    fixed last message issue.

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:Abn0rmal@localhost:5432/messenger", {
   logging: false
 });
 
