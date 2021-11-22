696188a24c64a05d4801f9234563d607ed9b1631
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-11-22 10:18:46 -0800

    updated database schema to allow group conversations

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,24 +1,25 @@
-const { Op } = require("sequelize");
+const { Op, ARRAY, INTEGER } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  otherUsersIds  : { type : ARRAY(INTEGER), defaultValue: null}
+});
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
+Conversation.findConversation = async function (user1Id, otherUsers) {
   const conversation = await Conversation.findOne({
     where: {
       user1Id: {
-        [Op.or]: [user1Id, user2Id]
+        [Op.or]: [user1Id]
       },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
+      otherUsersIds: {
+        [Op.or]: [otherUsers]
       }
     }
   });
 
-  // return conversation or null if it doesn't exist
   return conversation;
 };
 
